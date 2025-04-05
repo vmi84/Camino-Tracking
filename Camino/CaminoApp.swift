c9b7f7db98b0b8adaa9ff859cf5caced9547f43f
jwhite <vmi84@me.com>
2025-04-04 19:54:13 -0500

    Initial Commit

---
+++ Camino/CaminoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaminoApp.swift
+//  Camino
+//
+//  Created by Jeff White on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CaminoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaminoTests/CaminoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CaminoTests.swift
+//  CaminoTests
+//
+//  Created by Jeff White on 4/4/25.
+//
+
+import Testing
+
+struct CaminoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
